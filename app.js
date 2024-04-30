59180d5d2baf06a6dd7f387a38d8f3202673fdb4
Ehsan <139962810+Ehsan-Rabobi@users.noreply.github.com>
2024-04-30 08:03:59 +0330

    create basic route

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import userRouter from './routes/userRoute'
+import productRouter from './routes/productRoute'
+import commentRouter from './routes/commentRoute'
+import categoryRouter from './routes/categoryRoute'
+import orderRouter from './routes/orderRoute'
+import searchRouter from './routes/searchRoute'
 
 const app = express()
 app.use(morgan('dev'))
@@ -8,6 +14,12 @@ app.use(express.json())
 app.use(cors())
 
 // Routes
+app.use('/api/v1/user',userRouter)
+app.use('/api/v1/product',productRouter)
+app.use('/api/v1/comment',commentRouter)
+app.use('/api/v1/category',categoryRouter)
+app.use('/api/v1/order',orderRouter)
+app.use('/api/v1/search',searchRouter)
 
 
 
